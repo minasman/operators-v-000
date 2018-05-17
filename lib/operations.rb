ff0cafe4511a9cfa722f8a8a242eb59bf075b1c4
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-17 00:10:29 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
+require 'pry'
 def unsafe?(speed)
-
+  binding.pry
+  if speed > 40 && speed < 60
+    return FALSE
+  else 
+    return  TRUE
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 40 && speed < 60 ? false : true
 end
 	
 
